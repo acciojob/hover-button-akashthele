693f1fba8689b907367ffc3ea50bd12f5543772b
akashthele <120562656+akashthele@users.noreply.github.com>
2023-04-29 16:58:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+#btn{
+background-color:black;
+	color:white;
+	border-radius:5px;
+	border: 0px none white;
+}
+#btn:hover{
+	border-radius:30px;
+	background-color:yellow;
+	color:white;
+	
+	
+		  }
